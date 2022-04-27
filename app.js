8b014bd213b23c9543bf7456a4794c5e41cf7a3f
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-04-27 11:44:02 -0600

    done

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const colors = ['red', 'blue', 'green', 'yellow']
+const colors = ['red', 'blue', 'green', 'yellow','purple']
 
-console.log("============ Question 1 ============")
+console.log(colors[1])
+
 // Using its index value in colors, console.log 'blue'
 
 console.log("============ Question 2 ============")
 // add the value 'purple' to the end of the colors array
 // console.log the result
-
+console.log(colors[4])
 const benny = {
   name: 'Benny',
   age: 6,
@@ -14,17 +15,21 @@ const benny = {
 }
 
 console.log("============ Question 3 ============")
-// Using dot notation, console.log the name and age of Benny
-
+// Using dot notation, console.log the name and age of Benny\cons
+console.log(benny.name, benny.age);
 console.log("============ Question 4 ============")
 // Using dot notation, console.log any one of Benny's breeds
-
+console.log(benny.breeds[0]);
+for (let breed of benny.breeds) {
+  console.log(breed);
+}
 console.log("============ Question 5 ============")
 // Add code to the whisper function so that it takes whatever string is given to it
 // and returns an all lower case version
 
 function whisper(word){
   // Your code here
+  return word.toLowerCase();
 }
 
 console.log(`this should print true: ${whisper('Hello') === 'hello'}`)
